1102060427770d985be75124ff31a1657fd66196
patorma <pocontreras@ing.ucsc.cl>
2019-06-17 11:19:33 -0400

    al hacer click en un pokemon se muestra que tipo de pokemon es , su evolucion y las habilidades

--- src/app/pokemons/pokemon/pokemon-detalles/pokemon-detalles.component.ts
+++ src/app/pokemons/pokemon/pokemon-detalles/pokemon-detalles.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit,Input } from '@angular/core';
 import {ActivatedRoute} from '@angular/router'
-import { PokemonDetallesService  } from '../pokemon-detalles.service'
-import { Observable } from 'rxjs';
+import { PokemonsApiService  } from '.././shared/pokemons-api.service'
+import { Observable,forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-detalles',
@@ -9,18 +9,38 @@ import { Observable } from 'rxjs';
   styleUrls: ['./pokemon-detalles.component.css']
 })
 export class PokemonDetallesComponent implements OnInit {
+  private pokemon = { nombre: null, altura: null, peso: null, imagen: null };
+  private pokemonId;
 
-  @Input() name: any;
-  constructor(private _route:ActivatedRoute,private pokemonSvc:PokemonDetallesService ) {
-
+  @Input() indice:any;
+  constructor(private _route:ActivatedRoute,private pokemonSvc: PokemonsApiService) {
+    this.pokemonId =this._route.snapshot.paramMap.get('id')
    console.log(this._route.snapshot.paramMap.get('id'))
     console.log(name)
   }
-  df:any =this._route.snapshot.paramMap.get('id')
-  pokemon:Observable<any>
-
+  // df:any =this._route.snapshot.paramMap.get('id')
+  // pokemon:Observable<any>
+  pokemonForm : Observable<any>
+  pokemonH : Observable<any>
   ngOnInit() {
+    this.getPokemonForm()
+    this.getPokemonH()
     // this.getPokemon(this._route.snapshot.paramMap.get('id'));
+  }
+
+  getPokemonForm(){
+    this.pokemonForm = this.pokemonSvc.getPokemonForm(this.pokemonId)
+
+
+  }
+  getPokemonH(){
+    this.pokemonH = this.pokemonSvc.getPokemonH(this.pokemonId)
+  //   const h = () =>{
+  //     for(let r in this.pokemonH ){
+  //       this.pokemonH[r]
+  //   }
+  // }
+
   }
 
   // getPokemon(d:any){
--- src/app/pokemons/pokemon/shared/pokemons-api.service.ts
+++ src/app/pokemons/pokemon/shared/pokemons-api.service.ts
@@ -8,17 +8,22 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class PokemonsApiService {
-  DATO = '?limit=25'
-  URL_API = `https://pokeapi.co/api/v2/pokemon/`;
-  URL_API2 = `https://pokeapi.co/api/v2/pokemon/`;
+  DATO = 'pokemon/?limit=25'
+  FORM = 'pokemon-form/'
+  URL_API = 'https://pokeapi.co/api/v2/';
+  URL_API2 = 'https://pokeapi.co/api/v2/pokemon/';
   constructor(private http:HttpClient) { }
 
   getAllPokemons() : Observable<any>{
     return this.http.get<any>(this.URL_API+this.DATO)
     .pipe(map((data:any) => data.results))
   }
-  getPokemon(id:any):Observable<any>{
-    return this.http.get<any>(`${this.URL_API}${id}`)
+  getPokemonForm(pokemonId:any):Observable<any>{
+    return this.http.get<any>(`${this.URL_API2}${pokemonId}`)
     .pipe(map((data:any) => data.types))
+  }
+  getPokemonH(pokemonId:any):Observable<any>{
+    return this.http.get<any>(`${this.URL_API2}${pokemonId}`)
+    .pipe(map((data:any) => data.abilities))
   }
 }
